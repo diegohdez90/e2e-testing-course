1d1f9451df57b986848334e8818a32828f66b984
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-25 17:35:47 -0600

    test: open and close the modal

--- solution/02/cypress/e2e/tasks.cy.js
+++ solution/02/cypress/e2e/tasks.cy.js
@@ -1,5 +1,9 @@
 describe('tasks management', () => {
   it('should open and close the new tasks modal',  () => {
     cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click()
+    cy.get('.backdrop').click({
+      force: true
+    })
   })
 })
