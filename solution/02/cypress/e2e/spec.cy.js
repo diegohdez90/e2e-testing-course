136227eedd2f0aff027a9a66f675fc76d9d13135
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-25 17:16:17 -0600

    test: get h1 tag content

--- solution/02/cypress/e2e/spec.cy.js
+++ solution/02/cypress/e2e/spec.cy.js
@@ -4,5 +4,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
     cy.get('.main-header img');
+  });
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('h1').contains('React Tasks')
   })
 })
