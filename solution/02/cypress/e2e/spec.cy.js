6e757de619d0d82a59485a4eaadd20d3b77be912
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-25 17:23:53 -0600

    test: refactor over img tag in main-header class

--- solution/02/cypress/e2e/spec.cy.js
+++ solution/02/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
-    cy.get('.main-header img');
+    cy.get('.main-header').find('img')
   });
 
   it('should display the page title', () => {
