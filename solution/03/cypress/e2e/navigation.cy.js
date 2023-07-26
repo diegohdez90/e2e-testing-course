0fddedceb76dd5a6dbcaeb0656073eb801fb026c
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-26 17:01:08 -0600

    test: add navigation flow

--- solution/03/cypress/e2e/navigation.cy.js
+++ solution/03/cypress/e2e/navigation.cy.js
@@ -2,6 +2,13 @@
 
 describe('page navigation', () => {
   it('should navigate beetween pages', () => {
-    cy.get('[data-cy="header-about-link"]')
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-cy="header-about-link"]').click()
+    cy.location('pathname').should('eq', '/about');
+    cy.go('back');
+    cy.location('pathname').should('eq', '/');
+    cy.get('[data-cy="header-about-link"]').click()
+    cy.get('[data-cy="header-home-link"]').click()
+    cy.location('pathname').should('eq', '/');
   });
 });
