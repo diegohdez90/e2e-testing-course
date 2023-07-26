d2b6e548024caf05e8762d0e11b63dbed41a3069
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-25 18:17:41 -0600

    test: add filter flow

--- solution/02/cypress/e2e/tasks.cy.js
+++ solution/02/cypress/e2e/tasks.cy.js
@@ -34,4 +34,25 @@ describe('tasks management', () => {
     cy.get('.modal').contains('Add Task').click();
     cy.get('.error-message').should('exist').contains('Please provide values for task title, summary and category!')
   })
+
+  it('should filter tasks', () => {
+    cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click()
+    cy.get('#title').type('New Task');
+    cy.get('#summary').type('Some description');
+    cy.get('select#category').select('urgent')
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.backdrop').should('not.exist')
+    cy.get('.modal').should('not.exist')
+    cy.get('ul.task-list').find('li.task:last-child').find('h2').contains('New Task')
+    cy.get('ul.task-list').find('li.task:last-child').find('p').contains('Some description')
+    cy.get('select#filter').select('moderate')
+    cy.get('ul.task-list').should('have.length', 0)
+    cy.get('select#filter').select('urgent')
+    cy.get('ul.task-list').find('li.task:last-child').find('h2').contains('New Task')
+    cy.get('ul.task-list').find('li.task:last-child').find('p').contains('Some description')
+    cy.get('select#filter').select('all')
+    cy.get('ul.task-list').find('li.task:last-child').find('h2').contains('New Task')
+    cy.get('ul.task-list').find('li.task:last-child').find('p').contains('Some description')
+  })
 })
