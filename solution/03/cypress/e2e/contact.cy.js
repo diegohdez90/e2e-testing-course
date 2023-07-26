04f9fc830c949c7190af3ec97622b2438169741e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-26 17:16:42 -0600

    test: about contact form

--- solution/03/cypress/e2e/contact.cy.js
+++ solution/03/cypress/e2e/contact.cy.js
@@ -2,6 +2,14 @@
 
 describe('contact form', () => {
   it('should submit the form', () => {
-
+    cy.visit('http://localhost:5173/about')
+    cy.get('[data-cy="contact-input-message"]').type("Hi, Nice to meet you")
+    cy.get('[data-cy="contact-input-name"]').type('John Doe')
+    cy.get('[data-cy="contact-input-email"]').type('test@example.com')
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]').contains('Send Message')
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]').should('not.have.attr', 'disabled')
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]').click()
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]').contains('Sending...')
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]').should('have.attr', 'disabled')
   })
 })
