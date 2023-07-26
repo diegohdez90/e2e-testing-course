65555d7644269820d5d3247bb42566c8db1a93be
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-26 17:48:37 -0600

    test: refactor to add then callback

--- solution/03/cypress/e2e/contact.cy.js
+++ solution/03/cypress/e2e/contact.cy.js
@@ -6,13 +6,21 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').type("Hi, Nice to meet you")
     cy.get('[data-cy="contact-input-name"]').type('John Doe')
     cy.get('[data-cy="contact-input-email"]').type('test@example.com')
-    cy.get('[data-cy="contact-btn-submit"][type="submit"]')
-      .contains('Send Message')
-      .and('not.have.attr',  'disabled')
+    // cy.get('[data-cy="contact-btn-submit"][type="submit"]')
+    //   .contains('Send Message')
+    //   .and('not.have.attr',  'disabled')
+
+
     // const btn = cy.get('[data-cy="contact-btn-submit"][type="submit"]')
     // btn.click()
     // btn.contains('Sending...')
     // btn.should('have.attr', 'disabled')
+
+    cy.get('[data-cy="contact-btn-submit"][type="submit"]')
+      .then(el => {
+        expect(el.attr('disabled')).to.be.undefined;
+        expect(el.text()).to.be.eq('Send Message')
+      })
 
     cy.get('[data-cy="contact-btn-submit"][type="submit"]').as('submitBtn')
     cy.get('@submitBtn').click()
