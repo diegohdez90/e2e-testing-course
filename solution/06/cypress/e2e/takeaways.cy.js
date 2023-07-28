f2458b6f9d5a12f63f9a9705e24a305bb3628ec3
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-28 17:51:02 -0600

    test: add initial test

--- solution/06/cypress/e2e/takeaways.cy.js
+++ solution/06/cypress/e2e/takeaways.cy.js
@@ -3,5 +3,6 @@
 describe('Takeaways', () => {
   it('should display a list of fetched takeaways', () => {
     cy.visit('/')
+    cy.get('[data-cy="takeaway-item"]').should('have.length', 2)
   });
 });
