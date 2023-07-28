3330b485a65dbccef2d75f8a880fd41152c1a205
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-28 14:42:06 -0600

    test: add stub to retrieve location

--- solution/05/cypress/e2e/location.cy.js
+++ solution/05/cypress/e2e/location.cy.js
@@ -2,7 +2,11 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/')
+      .then(w => {
+        cy.stub(w.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
+      })
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called')
   });
 });
