fed51de4aefaa56bbc66ed2645ff488df24bb07d
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-28 15:14:37 -0600

    test: add stub to call clipboard text event

--- solution/05/cypress/e2e/location.cy.js
+++ solution/05/cypress/e2e/location.cy.js
@@ -16,6 +16,9 @@ describe('share location', () => {
               });
             }, 100);
           });
+        cy.stub(w.navigator.clipboard, 'writeText')
+          .as('saveToClipboard')
+          .resolves();
       });
   });
   it('should fetch the user location', () => {
@@ -28,5 +31,7 @@ describe('share location', () => {
   it('shoudl share a location URL', () => {
     cy.get('[data-cy="name-input"]').type('John Doe');
     cy.get('[data-cy="get-loc-btn"]').click()
+    cy.get('[data-cy="share-loc-btn"]').click()
+    cy.get('@saveToClipboard').should('have.been.called')
   });
 });
