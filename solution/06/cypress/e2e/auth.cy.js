8723ae66337f5cc9fa19c41de2df20e2002d098f
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-29 17:44:14 -0600

    test: add logout flow

--- solution/06/cypress/e2e/auth.cy.js
+++ solution/06/cypress/e2e/auth.cy.js
@@ -21,4 +21,17 @@ describe('Authentication flow', () => {
     cy.location('/pathname').should('eq', '/takeaways');
     cy.getCookie('__session').its('value').should('not.be.empty');
   })
+
+  it('should logout', () => {
+    cy.visit('/login');
+    cy.get('[data-cy="auth-email"]').click();
+    cy.get('[data-cy="auth-email"]').type('test@example.com');
+    cy.get('[data-cy="auth-password"]').type('testpassword');
+    cy.get('[data-cy="auth-submit"]').click();
+    cy.location('/pathname').should('eq', '/takeaways');
+    cy.getCookie('__session').its('value').should('not.be.empty');
+    cy.contains('Logout').click();
+    cy.location('/pathname').should('eq', '/');
+    cy.getCookie('__session').its('value').should('be.empty');
+  })
 });
