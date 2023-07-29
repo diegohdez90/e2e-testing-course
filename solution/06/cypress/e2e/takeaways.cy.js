c00d91ec70964de516e932d43ff825ec41a18416
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-28 18:28:23 -0600

    Revert "test: add seed database"
    This reverts commit 70a7d755e7b7fc7a8ce5ffb3d64bfd4c9b08d1b5.

--- solution/06/cypress/e2e/takeaways.cy.js
+++ solution/06/cypress/e2e/takeaways.cy.js
@@ -1,9 +1,6 @@
 /// <reference types="Cypress" />
 
 describe('Takeaways', () => {
-  beforeEach(() => {
-    cy.task('seedDatabase')
-  })
   it('should display a list of fetched takeaways', () => {
     cy.visit('/')
     cy.get('[data-cy="takeaway-item"]').should('have.length', 2)
