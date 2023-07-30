f09739f206d97b0109aaa240c1f2c5515b9962ff
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-07-29 18:00:04 -0600

    test: call add post takeaway endpoint

--- solution/06/cypress/e2e/takeaways.cy.js
+++ solution/06/cypress/e2e/takeaways.cy.js
@@ -8,4 +8,15 @@ describe('Takeaways', () => {
     cy.visit('/')
     cy.get('[data-cy="takeaway-item"]').should('have.length', 2)
   });
+
+  it('should add new takeaway', () => {
+    cy.intercept('POST', 'takeaways/new*').as('addTakeaway')
+    cy.login();
+    cy.visit('/takeaways/new');
+    cy.get('[data-cy="title"]').click();
+    cy.get('[data-cy="title"]').type('TestTitle1');
+    cy.get('[data-cy="body"]').type('TestBody1');
+    cy.get('[data-cy="create-takeaway"]').click();
+    cy.wait('@addTakeaway').its('request.body').should('match', /TestTitle1.*TestBody1/)
+  })
 });
